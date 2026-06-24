c29fd5373a753f6fb5df7d302c8a06033fc93ef8
Tolga Gümüş <hosgorensarp25@gmail.com>
2026-06-24 14:06:01 +0300

    Add favicon from logo-main.png

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -98,6 +98,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:card", content: "summary_large_image" },
     ],
     links: [
+      { rel: "icon", type: "image/png", href: "/model-oils/images/logo-main.png" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
